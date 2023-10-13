b74b77b1df33a662dbced3a407e4c70c7bf689b8
Ayush-Manware <ayushmanware19@gmail.com>
2023-10-13 22:27:12 +0530

    Bollywood Right Container

--- src/components/Fitness.jsx
+++ src/components/Fitness.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { useContext } from "react";
+import Store from "../store/Store";
+import { Link } from "react-router-dom";
 
 const Fitness = () => {
+  const [FitnessData] = useContext(Store);
+
+  const cate = "Fitness";
+
+  const filtered = FitnessData.filter((item) => item.category === cate);
+
   return (
     <>
-     <h1>Fitness</h1>
-    </>
-  )
-}
+      <div className="bollywoodContainer">
+        <div className="left">
+          <h2 className="bollywoodHeading">Fitness</h2>
+          {filtered.map((item, index) => (
+            <>
+              <Link to={`/info/${item.id}`} >
+                <div className="cardContainer">
+                  <div className="cardImageContainer">
+                    <img className="cardImage" src={item.image} alt="Error" />
+                  </div>
+                  <div className="cardContent">
+                    <h2>{item.name}</h2>
+                    <p>{item.text.slice(0, 130)}...</p>
+                  </div>
+                </div>
+              </Link>
+            </>
+          ))}
+        </div>
 
-export default Fitness
+        <div className="right">
+          <h2>Right</h2>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Fitness;
