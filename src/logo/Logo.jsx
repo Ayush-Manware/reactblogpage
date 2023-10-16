3d31800773bb0dede6fb68716d8b3e6fa341845f
Ayush-Manware <ayushmanware19@gmail.com>
2023-10-16 13:52:56 +0530

    Dynamic UI

--- src/logo/Logo.jsx
+++ src/logo/Logo.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import logoImage from './two.png'; 
+import menu from "./menu_5259008.png"
 
 const Logo = () => {
   return (
     <div className='logoContainer'>
       <img className='logoImage' src={logoImage} alt="Logo" />
+      <img className='menuIcon' src={menu} alt="Err" />
     </div>
   );
 };
--- src/navbar/Dynamic.jsx
+++ src/navbar/Dynamic.jsx
@@ -1,27 +1,42 @@
 import React, { useContext } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import Store from "../store/Store";
+import like from "./like.png";
+import share from "./share (1).png";
 
 const Dynamic = () => {
   const id = useParams().id;
   const [dataFromStore] = useContext(Store);
   const navigation = useNavigate();
 
-  return(
+  return (
     <>
-      {
-      dataFromStore.filter((data) => data.id === parseInt(id))
-      .map((item,index) => {
-        return(
+      {dataFromStore
+        .filter((data) => data.id === parseInt(id))
+        .map((item, index) => {
+          return (
             <>
-             <h2>{item.name}</h2>
-             <img src={item.image} alt="err" />
-             <p>{item.text}</p>
-             <button onClick={()=> navigation(-1)}>Go Back</button>
+              <div className="dynamicContainer">
+                <div className="dynamicLeft">
+                  <div className="likeContainer">
+                    <img src={like} className="likeImage" alt="Err-/" />
+                    <h3>Like</h3>
+                  </div>
+                  <div className="shareContainer">
+                    <img src={share} className="shareImage" alt="Err-/" />
+                    <h3>Share thi article</h3>
+                  </div>
+                </div>
+                <div className="dynamicRight">
+                  <h2>{item.name}</h2>
+                  <img src={item.image} alt="err" />
+                  <p>{item.text}</p>
+                  <button onClick={() => navigation(-1)}>Go Back</button>
+                </div>
+              </div>
             </>
-        )
-      })
-      }
+          );
+        })}
     </>
   );
 };
